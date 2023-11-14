68eeccacc150d9be109faee67214bf17d035dbc5
baekboowoo <131091538+baekboowoo@users.noreply.github.com>
2023-11-14 13:51:58 +0900

    Update DataSourceConfig.java

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -28,7 +28,7 @@ public class DataSourceConfig {
 	{
 		HikariDataSource dataSource = new HikariDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setJdbcUrl("jdbc:mysql://3.38.36.12:3306/daeguyo");
+		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/daeguyo");
 		dataSource.setUsername("root");
 		dataSource.setPassword("212302*qorAh/");
 		
